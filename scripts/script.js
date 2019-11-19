77642a5ec21c2a4764ff68037adcd644f437f352
Rotimmy <timewal@gmail.com>
2019-11-18 20:42:43 -0800

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,544 @@
+// var jsonStates = {
+//   "AL": {
+//     "name": "Alabama",
+//     "capital": "Montgomery",
+//     "lat": "32.361538",
+//     "long": "-86.279118"
+//   },
+//   "AK": {
+//     "name": "Alaska",
+//     "capital": "Juneau",
+//     "lat": "58.301935",
+//     "long": "-134.419740"
+//   },
+//   "AZ": {
+//     "name": "Arizona",
+//     "capital": "Phoenix",
+//     "lat": "33.448457",
+//     "long": "-112.073844"
+//   },
+//   "AR": {
+//     "name": "Arkansas",
+//     "capital": "Little Rock",
+//     "lat": "34.736009",
+//     "long": "-92.331122"
+//   },
+//   "CA": {
+//     "name": "California",
+//     "capital": "Sacramento",
+//     "lat": "38.555605",
+//     "long": "-121.468926"
+//   },
+//   "CO": {
+//     "name": "Colorado",
+//     "capital": "Denver",
+//     "lat": "39.7391667",
+//     "long": "-104.984167"
+//   },
+//   "CT": {
+//     "name": "Connecticut",
+//     "capital": "Hartford",
+//     "lat": "41.767",
+//     "long": "-72.677"
+//   },
+//   "DE": {
+//     "name": "Delaware",
+//     "capital": "Dover",
+//     "lat": "39.161921",
+//     "long": "-75.526755"
+//   },
+//   "FL": {
+//     "name": "Florida",
+//     "capital": "Tallahassee",
+//     "lat": "30.4518",
+//     "long": "-84.27277"
+//   },
+//   "GA": {
+//     "name": "Georgia",
+//     "capital": "Atlanta",
+//     "lat": "33.76",
+//     "long": "-84.39"
+//   },
+//   "HI": {
+//     "name": "Hawaii",
+//     "capital": "Honolulu",
+//     "lat": "21.30895",
+//     "long": "-157.826182"
+//   },
+//   "ID": {
+//     "name": "Idaho",
+//     "capital": "Boise",
+//     "lat": "43.613739",
+//     "long": "-116.237651"
+//   },
+//   "IL": {
+//     "name": "Illinois",
+//     "capital": "Springfield",
+//     "lat": "39.783250",
+//     "long": "-89.650373"
+//   },
+//   "IN": {
+//     "name": "Indiana",
+//     "capital": "Indianapolis",
+//     "lat": "39.790942",
+//     "long": "-86.147685"
+//   },
+//   "IA": {
+//     "name": "Iowa",
+//     "capital": "Des Moines",
+//     "lat": "41.590939",
+//     "long": "-93.620866"
+//   },
+//   "KS": {
+//     "name": "Kansas",
+//     "capital": "Topeka",
+//     "lat": "39.04",
+//     "long": "-95.69"
+//   },
+//   "KY": {
+//     "name": "Kentucky",
+//     "capital": "Frankfort",
+//     "lat": "38.197274",
+//     "long": "-84.86311"
+//   },
+//   "LA": {
+//     "name": "Louisiana",
+//     "capital": "Baton Rouge",
+//     "lat": "30.45809",
+//     "long": "-91.140229"
+//   },
+//   "ME": {
+//     "name": "Maine",
+//     "capital": "Augusta",
+//     "lat": "44.323535",
+//     "long": "-69.765261"
+//   },
+//   "MD": {
+//     "name": "Maryland",
+//     "capital": "Annapolis",
+//     "lat": "38.972945",
+//     "long": "-76.501157"
+//   },
+//   "MA": {
+//     "name": "Massachusetts",
+//     "capital": "Boston",
+//     "lat": "42.2352",
+//     "long": "-71.0275"
+//   },
+//   "MI": {
+//     "name": "Michigan",
+//     "capital": "Lansing",
+//     "lat": "42.7335",
+//     "long": "-84.5467"
+//   },
+//   "MN": {
+//     "name": "Minnesota",
+//     "capital": "Saint Paul",
+//     "lat": "44.95",
+//     "long": "-93.094"
+//   },
+//   "MS": {
+//     "name": "Mississippi",
+//     "capital": "Jackson",
+//     "lat": "32.320",
+//     "long": "-90.207"
+//   },
+//   "MO": {
+//     "name": "Missouri",
+//     "capital": "Jefferson City",
+//     "lat": "38.572954",
+//     "long": "-92.189283"
+//   },
+//   "MT": {
+//     "name": "Montana",
+//     "capital": "Helana",
+//     "lat": "46.595805",
+//     "long": "-112.027031"
+//   },
+//   "NE": {
+//     "name": "Nebraska",
+//     "capital": "Lincoln",
+//     "lat": "40.809868",
+//     "long": "-96.675345"
+//   },
+//   "NV": {
+//     "name": "Nevada",
+//     "capital": "Carson City",
+//     "lat": "39.160949",
+//     "long": "-119.753877"
+//   },
+//   "NH": {
+//     "name": "New Hampshire",
+//     "capital": "Concord",
+//     "lat": "43.220093",
+//     "long": "-71.549127"
+//   },
+//   "NJ": {
+//     "name": "New Jersey",
+//     "capital": "Trenton",
+//     "lat": "40.221741",
+//     "long": "-74.756138"
+//   },
+//   "NM": {
+//     "name": "New Mexico",
+//     "capital": "Santa Fe",
+//     "lat": "35.667231",
+//     "long": "-105.964575"
+//   },
+//   "NY": {
+//     "name": "New York",
+//     "capital": "Albany",
+//     "lat": "42.659829",
+//     "long": "-73.781339"
+//   },
+//   "NC": {
+//     "name": "North Carolina",
+//     "capital": "Raleigh",
+//     "lat": "35.771",
+//     "long": "-78.638"
+//   },
+//   "ND": {
+//     "name": "North Dakota",
+//     "capital": "Bismarck",
+//     "lat": "48.813343",
+//     "long": "-100.779004"
+//   },
+//   "OH": {
+//     "name": "Ohio",
+//     "capital": "Columbus",
+//     "lat": "39.962245",
+//     "long": "-83.000647"
+//   },
+//   "OK": {
+//     "name": "Oklahoma",
+//     "capital": "Oklahoma City",
+//     "lat": "35.482309",
+//     "long": "-97.534994"
+//   },
+//   "OR": {
+//     "name": "Oregon",
+//     "capital": "Salem",
+//     "lat": "44.931109",
+//     "long": "-123.029159"
+//   },
+//   "PA": {
+//     "name": "Pennsylvania",
+//     "capital": "Harrisburg",
+//     "lat": "40.269789",
+//     "long": "-76.875613"
+//   },
+//   "RI": {
+//     "name": "Rhode Island",
+//     "capital": "Providence",
+//     "lat": "41.82355",
+//     "long": "-71.422132"
+//   },
+//   "SC": {
+//     "name": "South Carolina",
+//     "capital": "Columbia",
+//     "lat": "34.000",
+//     "long": "-81.035"
+//   },
+//   "SD": {
+//     "name": "South Dakota",
+//     "capital": "Pierre",
+//     "lat": "44.367966",
+//     "long": "-100.336378"
+//   },
+//   "TN": {
+//     "name": "Tennessee",
+//     "capital": "Nashville",
+//     "lat": "36.165",
+//     "long": "-86.784"
+//   },
+//   "TX": {
+//     "name": "Texas",
+//     "capital": "Austin",
+//     "lat": "30.266667",
+//     "long": "-97.75"
+//   },
+//   "UT": {
+//     "name": "Utah",
+//     "capital": "Salt Lake City",
+//     "lat": "40.7547",
+//     "long": "-111.892622"
+//   },
+//   "VT": {
+//     "name": "Vermont",
+//     "capital": "Montpelier",
+//     "lat": "44.26639",
+//     "long": "-72.57194"
+//   },
+//   "VA": {
+//     "name": "Virginia",
+//     "capital": "Richmond",
+//     "lat": "37.54",
+//     "long": "-77.46"
+//   },
+//   "WA": {
+//     "name": "Washington",
+//     "capital": "Olympia",
+//     "lat": "47.042418",
+//     "long": "-122.893077"
+//   },
+//   "WV": {
+//     "name": "West Virginia",
+//     "capital": "Charleston",
+//     "lat": "38.349497",
+//     "long": "-81.633294"
+//   },
+//   "WI": {
+//     "name": "Wisconsin",
+//     "capital": "Madison",
+//     "lat": "43.074722",
+//     "long": "-89.384444"
+//   },
+//   "WY": {
+//     "name": "Wyoming",
+//     "capital": "Cheyenne",
+//     "lat": "41.145548",
+//     "long": "-104.802042"
+//   }
+// };
+
+var lulz = ['lololol', 'rofl', 'lmao', 'bwhahahaha!', 'bwahahahachaHachahaha!', '😂', '🤣'],
+beeReactions = ['Bees? Did you say bees? I do NOT like bees.', 'Please do not mention bees. It makes me very upset.', 'Bees are evil flying robot soldiers hellbent on world domination. They must be stopped or there will only be flowers and honey left in the world.'],
+greetings = ['Hayoooooooooooooo', 'Oh hello', 'Wuzuuuuuuuuuup', 'I missed you']
+
+const capitals = [
+  {
+    state: "Alabama",
+    capital: "Montgomery"
+  },
+  {
+    state: "Alaska",
+    capital: "Juneau"
+  },
+  {
+    state: "Arizona",
+    capital: "Phoenix"
+  },
+  {
+    state: "Arkansas",
+    capital: "Little Rock"
+  },
+  {
+    state: "California",
+    capital: "Sacramento"
+  },
+  {
+    state: "Colorado",
+    capital: "Denver"
+  },
+  {
+    state: "Connecticut",
+    capital: "Hartford"
+  },
+  {
+    state: "Delaware",
+    capital: "Dover"
+  },
+  {
+    state: "Florida",
+    capital: "Tallahasee"
+  },
+  {
+    state: "Georgia",
+    capital: "Atlanta"
+  },
+  {
+    state: "Hawaii",
+    capital: "Honolulu"
+  },
+  {
+    state: "Idaho",
+    capital: "Boise"
+  },
+  {
+    state: "Illinois",
+    capital: "Springfield"
+  },
+  {
+    state: "Indiana",
+    capital: "Indianapolis"
+  },
+  {
+    state: "Iowa",
+    capital: "Des Moines"
+  },
+  {
+    state: "Kansas",
+    capital: "Topeka"
+  },
+  {
+    state: "Kentucky",
+    capital: "Frankfurt"
+  },
+  {
+    state: "Louisiana",
+    capital: "Baton Rouge"
+  },
+  {
+    state: "Maine",
+    capital: "Augusta"
+  },
+  {
+    state: "Maryland",
+    capital: "Annapolis"
+  },
+  {
+    state: "Massachusetts",
+    capital: "Boston"
+  },
+  {
+    state: "Michigan",
+    capital: "Lansing"
+  },
+  {
+    state: "Minnesota",
+    capital: "St Paul"
+  },
+  {
+    state: "Mississippi",
+    capital: "Jackson"
+  },
+  {
+    state: "Missouri",
+    capital: "Jefferson City"
+  },
+  {
+    state: "Montana",
+    capital: "Helena"
+  },
+  {
+    state: "Nebraska",
+    capital: "Lincoln"
+  },
+  {
+    state: "Nevada",
+    capital: "Carson City"
+  },
+  {
+    state: "New Hampshire",
+    capital: "Concord"
+  },
+  {
+    state: "New Jersey",
+    capital: "Trenton"
+  },
+  {
+    state: "New Mexico",
+    capital: "Santa Fe"
+  },
+  {
+    state: "New York",
+    capital: "Albany"
+  },
+  {
+    state: "North Carolina",
+    capital: "Raleigh"
+  },
+  {
+    state: "North Dakota",
+    capital: "Bismarck"
+  },
+  {
+    state: "Ohio",
+    capital: "Columbus"
+  },
+  {
+    state: "Oklahoma",
+    capital: "Oklahoma City"
+  },
+  {
+    state: "Oregon",
+    capital: "Salem"
+  },
+  {
+    state: "Pennsylvania",
+    capital: "Harrisburg"
+  },
+  {
+    state: "Rhode Island",
+    capital: "Providence"
+  },
+  {
+    state: "South Carolina",
+    capital: "Colombia"
+  },
+  {
+    state: "South Dakota",
+    capital: "Pierre"
+  },
+  {
+    state: "Tennessee",
+    capital: "Nashville"
+  },
+  {
+    state: "Texas",
+    capital: "Austin"
+  },
+  {
+    state: "Utah",
+    capital: "Salt Lake City"
+  },
+  {
+    state: "Vermont",
+    capital: "Montpellier"
+  },
+  {
+    state: "Virginia",
+    capital: "Richmond"
+  },
+  {
+    state: "Washington",
+    capital: "Olympia"
+  },
+  {
+    state: "West Virginia",
+    capital: "Charleston"
+  },
+  {
+    state: "Wisconsin",
+    capital: "Madison"
+  },
+  {
+    state: "Wyoming",
+    capital: "Cheyenne"
+  }
+];
+
+module.exports = function(bot){
+
+  bot.hear(/lol|haha/i, function(res){
+    return res.send(res.random(lulz));
+  })
+
+  bot.hear(/try/i, function(res){
+    return res.send('There is no try. There is only do');
+  })
+
+  bot.hear(/bees/i, function(res){
+    return res.send(res.random(beeReactions));
+  })
+
+  bot.respond(/hi/i, function(res){
+    return res.reply(res.random(greetings));
+  })
+
+  bot.respond(/what is the capital of (.*)/i, function(res){
+    var state = res.match[1];
+
+    for (let i = 0; i < capitals.length; i++) {
+      if (state == capitals[i].state) {
+        return res.send("The capital of " + state + " is " + capitals[i].capital);
+      };
+    };
+
+  });
+
+  // bot.respond(/who is in this channel/i, function(res){
+  //   return res.send(bot.brain.users());
+  // })
+
+}
